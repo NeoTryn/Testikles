25c86c4a59ec713c7a6ba90e47b167518c04ed6e
NeoTryn <felixeidler@gmail.com>
2025-11-05 11:57:01 +0100

    assertEquals added

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@ namespace testikles {
 	
 	#ifndef RESULT_STRINGS
 	#define RESULT_STRINGS
-	#define OK "\033[32m| OK |\033[0m"
+	#define OK "\033[32m|  OK  |\033[0m"
 	#define FAIL "\033[31m| FAIL |\033[0m"  
 	#endif
 
-	static void printResult(std::string strToPrint, bool success) {
+	static inline void printResult(std::string strToPrint, bool success) {
 		if (success) {
 			std::cout << OK << " " << strToPrint << std::endl;
 		}
@@ -29,14 +29,24 @@ namespace testikles {
 				printResult(strToPrint, true);
 			}
 			else {
-				std::string strToPrint = actual + " was returned instead of " + expected + ".\n";
+				std::string strToPrint = actual + " was returned instead of " + expected + ".";
 
 				printResult(strToPrint, false);
 			}
-
 		}
 		catch(const std::exception& e) {
 			std::cout << "Error: " << e.what() << std::endl;			
+		}
+	}
+
+	template<typename V>
+	void assertEquals(V value, V expected) {
+		if (value == expected) {
+			printResult("The values match.", true);
+		}
+		else {
+			std::string strToPrint = value + " and " + expected + " do not match.";
+			printResult(strToPrint, false);
 		}
 	}
 }
@@ -47,5 +57,10 @@ std::string print() {
 
 int main() {
 	std::string hello = "HELLO2";
+	std::string hell2 = "HELLO2";
+	std::string hell3 = "HELLO3";
+	int bye = 1;
+
 	testikles::assertReturns(print, hello);
+	testikles::assertEquals(hello, hell2);
 }
