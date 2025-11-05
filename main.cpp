81c1876b7c94ce52a5cb4f7244c6ae8782f85314
NeoTryn <felixeidler@gmail.com>
2025-11-05 11:11:34 +0100

    Refactored methods

--- main.cpp
+++ main.cpp
@@ -3,21 +3,40 @@
 
 namespace testikles {
 	
+	#ifndef RESULT_STRINGS
+	#define RESULT_STRINGS
+	#define OK "\033[32m| OK |\033[0m"
+	#define FAIL "\033[31m| FAIL |\033[0m"  
+	#endif
+
+	static void printResult(std::string strToPrint, bool success) {
+		if (success) {
+			std::cout << OK << " " << strToPrint << std::endl;
+		}
+		else {
+			std::cout << FAIL << " " << strToPrint << std::endl;
+		}
+	}
+
 	template<typename F, typename R>
-	void test(F func, R expected) {
+	void assertReturns(F func, R expected) {
 		try {
 			R actual = func();
 
 			if (actual == expected) {
-				std::cout << "\033[32m| OK |\033[0m" << " " << __func__ << " returned expected value.\n";
+				std::string strToPrint = expected + " was returned";
+					
+				printResult(strToPrint, true);
 			}
 			else {
-				std::cout << "\033[31m| FAIL |\033[0m" << " " << __func__ << " returned " << actual << " instead of " << expected << ".\n";
+				std::string strToPrint = actual + " was returned instead of " + expected + ".\n";
+
+				printResult(strToPrint, false);
 			}
 
 		}
 		catch(const std::exception& e) {
-			std::cout << "Runtime Error: " << e.what() << std::endl;			
+			std::cout << "Error: " << e.what() << std::endl;			
 		}
 	}
 }
@@ -28,5 +47,5 @@ std::string print() {
 
 int main() {
 	std::string hello = "HELLO2";
-	testikles::test(print, hello);
+	testikles::assertReturns(print, hello);
 }
